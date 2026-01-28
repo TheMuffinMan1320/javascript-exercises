c8b3bd97170d2ab9845b8aee8ccb1ba470ea687e
TheMuffinMan1320 <76186189+TheMuffinMan1320@users.noreply.github.com>
2026-01-28 11:58:58 -0600

    did some js practice with strings and arrays

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let sum = 0
+    let lower = a < b ? a : b
+    let larger = a > b ? a : b
+    if (!Number.isInteger(a) || !Number.isInteger(b) || a < 0 || b < 0) {
+        return "ERROR"
+    }
+    for (let i = lower; i <= larger; i++) {
+        sum += i
+    }
+    return sum
 };
 
 // Do not edit below this line
